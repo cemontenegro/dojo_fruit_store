91a3696769732197a8ad5a9bb94d4bd457becff4
Carolina Montenegro <arq.cmontenegro@gmail.com>
2022-09-28 19:58:46 -0300

    _

--- server.py
+++ server.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template, request, redirect
-app = Flask(__name__)  
+app = Flask(__name__)
 
-@app.route('/')         
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+@app.route('/checkout', methods=['POST'])
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    data = {
+        "frutilla":int(request.form['strawberry']),
+        "mora":int(request.form['raspberry']),
+        "manzana":int(request.form['apple']),
+        "nombre":request.form['first_name'],
+        "apellido":request.form['last_name'],
+        "identificacion":request.form['student_id'],
+    }
+    suma_total = data["frutilla"]+data["mora"]+data["manzana"]
+    print ("Cobrando a",data["nombre"],data["apellido"],"por",suma_total, "frutas")
+    return render_template("checkout.html", data= data, suma_total=suma_total)
 
-@app.route('/fruits')         
+@app.route('/fruits')
 def fruits():
     return render_template("fruits.html")
 
-if __name__=="__main__":   
-    app.run(debug=True)    
+if __name__=="__main__":
+    app.run(debug=True)
